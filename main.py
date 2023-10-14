ea77698b67a1d9966aa0b80deee24f0d89492958
jpforbes5151 <jpforbes5151@gmail.com>
2023-10-13 20:10:30 -0400

    added comments and defined methods from logic

--- main.py
+++ main.py
@@ -1,41 +1,47 @@
-# reading the file and printing out the entirety of the text
-    path_to_book = "/home/jpforbes/workspace/github.com/jpforbes5151/bookbot/books/frankenstein.txt"
-    with open(path_to_book) as f:
-        file_contents = f.read()
 
-# splitting the text into words
+def main():
+    path_to_book = "books/frankenstein.txt"
+    file_contents = get_book_text(path_to_book)
+    words = get_word_count(file_contents)
+    file_contents_lower = file_contents.lower()
+    character_dictionary  = get_char_dict(file_contents_lower)
 
-words = file_contents.split()
-#print(words[0])
+    #filters dictionary for only alphabetical characters
+    filtered_dict = {key: value for key, value in character_dictionary.items() if key.isalpha()}
 
-#count words in all of the text
+    #sortes dictionary into ascending order
+    sorted_filtered_dict = sorted(filtered_dict.items(), key=lambda item: item[1], reverse = True)
+    
 
-character_dict = {}
-file_contents_lower = file_contents.lower()
-for letter in file_contents_lower:
-    if letter in character_dict:
-        character_dict[letter] += 1
-    else:
-        character_dict[letter] = 1
+    print(f"--- Begin report of {path_to_book} ---")
+    print(f"There are {words} words in this book.")
 
-# sorting and removing non-needed characters
+    for character, number in sorted_filtered_dict:
+        print(f"The '{character}' character was found '{number}' times ")
+    print(f"---- End Report ----")
 
-# Filtering the dictionary to remove non-alphabet characters
-filtered_dict = {key: value for key, value in character_dict.items() if key.isalpha()}
+# pulls text from text file
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
 
-# sorting the filtered dictionary in descending order
-sorted_filtered_dict = sorted(filtered_dict.items(), key=lambda item: item[1], reverse = True)
+# makes a dictionary of each character within the text
+def get_char_dict(text):
+    character_dict = {}
+    for letter in text:
+        if letter in character_dict:
+            character_dict[letter] += 1
+        else:
+            character_dict[letter] = 1
+    return character_dict
+
+# returns the word count of the book
+def get_word_count(text):
+    words = text.split()
+    return len(words)
 
 
-# Printing report
-
-print(f"--- Begin report of books/frankenstein.txt ---")
-for character, number in sorted_filtered_dict:
-    print(f"The '{character}' character was found '{number}' times ")
-print(f"---- End Report ----")
-
-
-
+main()
 
 
 
